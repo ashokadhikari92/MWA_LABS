f0542182235439309e6696e2a4b85ded5409054a
unknown <986716@CS.MUM.EDU>
2018-11-30 17:11:00 -0600

    Working on Lab5 Exercise1

---
+++ lab5/exercise1/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.set('x-powered-by',false);
+app.enable('strict routing');
+app.enable('case sensitive routing');
+app.enable('trust proxy');
+
+
+app.get('/users', (req,res)=>{
+    axios.get('https://randomuser.me/api/?results=10')
+    .then((data)=>{
+        console.log(data);
+        res.writeHead(200, {'Content-Type':'application/json'});
+        res.end(JSON.stringify(data.data));
+    })
+    .catch(error => {
+        res.end(error.message);
+    });
+});
+
+app.listen(3000, () => `Listening on 3000 port.`);
