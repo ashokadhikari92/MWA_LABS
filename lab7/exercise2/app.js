40b99473cd3d4a11cd996e609f86104350a1a29e
Ashok Adhikari <ashok.adhikari92@gmail.com>
2018-12-03 15:51:49 -0600

    Extracted routes in a separate js file

--- lab7/exercise2/app.js
+++ lab7/exercise2/app.js
@@ -1,39 +1,21 @@
 const express = require('express');
 const app = express();
+const secretRoutes = require('./routes/secret');
 
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient('mongodb://localhost:27017');
 
 app.set('x-powered-by',false);
-app.enable('strict routing');
 app.enable('case sensitive routing');
 app.enable('trust proxy');
 
-const crypto = require('crypto'),
-    algorithm = 'aes256',
-    password = 'asaadsaad';
+
+app.use('/secret', secretRoutes);
 
 
-const decrypt = function(text){
-    var decipher = crypto.createDecipher(algorithm,password)
-    var decriptedText = decipher.update(text,'hex','utf8')
-    decriptedText += decipher.final('utf8');
-    return decriptedText;
-}
+client.connect(function(error){
+    app.locals.db = client.db('week2');
+    const port = 3000;
+    app.listen(port, () => console.log(`Listening on ${port} port.`));
+})
 
-app.get('/secret', (req,res, next)=>{
-
-    client.connect(function(error){
-        const collection = client.db('week2').collection('week2.homework7');
-    
-        const doc = collection.findOne({});
-        doc.then((value) => {
-            const decriptedText = decrypt(value.message);
-            res.status(200).json(decriptedText);
-        });
-    })
-
-});
-
-const port = 3000;
-app.listen(port, () => console.log(`Listening on ${port} port.`));
