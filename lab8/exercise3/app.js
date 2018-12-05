7e5f6635a5796782438dae072208bd9256404ecd
Ashok Adhikari <ashok.adhikari92@gmail.com>
2018-12-05 01:26:58 -0600

    Working on lab8 exercise3

---
+++ lab8/exercise3/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const app = express();
+const locationRoutes = require('./routes/location');
+const cors = require('cors');
+
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient('mongodb://localhost:27017');
+// const client = new MongoClient('mongodb://root:root123@ds125953.mlab.com:25953');
+
+app.set('x-powered-by',false);
+app.enable('case sensitive routing');
+app.enable('trust proxy');
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use('/api/locations', locationRoutes);
+
+
+client.connect(function(error){
+    app.locals.db = client.db('lab8');
+    const port = 3600;
+    app.listen(port, () => console.log(`Listening on ${port} port.`));
+})
+
